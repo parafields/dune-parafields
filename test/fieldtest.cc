14aac8c6f0c0d8c0ac6dc2e4d86fb819e752fda2
Ole Klein <ole.klein@iwr.uni-heidelberg.de>
2016-07-25 19:12:03 +0200

    split test cases into different binaries

--- test/fieldtest.cc
+++ test/fieldtest.cc
@@ -12,6 +12,9 @@
 #include<dune/common/fvector.hh>
 #include<dune/randomfield/randomfield.hh>
 
+/**
+ * @brief Types for coordinates and range values
+ */
 template<typename DF, typename RF, unsigned int dimension>
 class GridTraits
 {
@@ -19,25 +22,179 @@ class GridTraits
 
     enum {dim = dimension};
 
-    //typedef Dune::YaspGrid<dim> Grid;
-    //typedef typename Grid::LevelGridView GridView;
-
-    typedef RF                              RangeField;
-    typedef Dune::FieldVector<RF,1>         Scalar;
-    typedef Dune::FieldVector<RF,dim>       Vector;
-    typedef Dune::FieldMatrix<RF,dim,dim>   Tensor;
-    typedef DF                              DomainField;
-    typedef Dune::FieldVector<DF,dim>       Domain;
-    typedef Dune::FieldVector<DF,dim-1>     IntersectionDomain;
-    //typedef typename GridView::Traits::template Codim<0>::Entity Element;
-    //typedef typename GridView::Intersection Intersection;
-
+    typedef RF                        RangeField;
+    typedef Dune::FieldVector<RF,1>   Scalar;
+    typedef DF                        DomainField;
+    typedef Dune::FieldVector<DF,dim> Domain;
 };
+
+/**
+ * @brief Test effect of (I - M^1/2 M^-1 M^1/2)
+ */
+template<typename RandomFieldList>
+void diffRootInvRoot(const RandomFieldList& fieldList)
+{
+  const double fieldNorm = std::sqrt(fieldList * fieldList);
+  RandomFieldList copy(fieldList);
+  copy.timesMatrixRoot();
+  copy.timesInverseMatrix();
+  copy.timesMatrixRoot();
+  copy -= fieldList;
+  const double diffNorm = std::sqrt(copy * copy);
+
+  std::cout << "root inv root";
+  std::cout << " norm: " << fieldNorm;
+  std::cout << " diffNorm: " << diffNorm;
+  std::cout << " opNorm: " << diffNorm/fieldNorm << std::endl;
+}
+
+/**
+ * @brief Test effect of (I - M^-1 M^1/2 M^1/2)
+ */
+template<typename RandomFieldList>
+void diffRootRootInv(const RandomFieldList& fieldList)
+{
+  const double fieldNorm = std::sqrt(fieldList * fieldList);
+  RandomFieldList copy(fieldList);
+  copy.timesMatrixRoot();
+  copy.timesMatrixRoot();
+  copy.timesInverseMatrix();
+  copy -= fieldList;
+  const double diffNorm = std::sqrt(copy * copy);
+
+  std::cout << "root root inv";
+  std::cout << " norm: " << fieldNorm;
+  std::cout << " diffNorm: " << diffNorm;
+  std::cout << " opNorm: " << diffNorm/fieldNorm << std::endl;
+}
+
+/**
+ * @brief Test effect of (I - M^1/2 M^1/2 M^-1)
+ */
+template<typename RandomFieldList>
+void diffInvRootRoot(const RandomFieldList& fieldList)
+{
+  const double fieldNorm = std::sqrt(fieldList * fieldList);
+  RandomFieldList copy(fieldList);
+  copy.timesInverseMatrix();
+  copy.timesMatrixRoot();
+  copy.timesMatrixRoot();
+  copy -= fieldList;
+  const double diffNorm = std::sqrt(copy * copy);
+
+  std::cout << "inv root root";
+  std::cout << " norm: " << fieldNorm;
+  std::cout << " diffNorm: " << diffNorm;
+  std::cout << " opNorm: " << diffNorm/fieldNorm << std::endl;
+}
+
+/**
+ * @brief Test effect of (I - M M^-1)
+ */
+template<typename RandomFieldList>
+void diffInvMult(const RandomFieldList& fieldList)
+{
+  const double fieldNorm = std::sqrt(fieldList * fieldList);
+  RandomFieldList copy(fieldList);
+  copy.timesInverseMatrix();
+  copy.timesMatrix();
+  copy -= fieldList;
+  const double diffNorm = std::sqrt(copy * copy);
+
+  std::cout << "inv mult     ";
+  std::cout << " norm: " << fieldNorm;
+  std::cout << " diffNorm: " << diffNorm;
+  std::cout << " opNorm: " << diffNorm/fieldNorm << std::endl;
+}
+
+/**
+ * @brief Test effect of (I - M^-1 M)
+ */
+template<typename RandomFieldList>
+void diffMultInv(const RandomFieldList& fieldList)
+{
+  const double fieldNorm = std::sqrt(fieldList * fieldList);
+  RandomFieldList copy(fieldList);
+  copy.timesMatrix();
+  copy.timesInverseMatrix();
+  copy -= fieldList;
+  const double diffNorm = std::sqrt(copy * copy);
+
+  std::cout << "mult inv     ";
+  std::cout << " norm: " << fieldNorm;
+  std::cout << " diffNorm: " << diffNorm;
+  std::cout << " opNorm: " << diffNorm/fieldNorm << std::endl;
+}
+
+/**
+ * @brief Run different matrix multiplication tests
+ */
+template<typename GridTraits, typename Covariance, bool storeInvMat, bool storeInvRoot>
+void runTests(const Dune::ParameterTree& config)
+{
+  Dune::RandomField::RandomFieldList<GridTraits,Covariance,storeInvMat,storeInvRoot> randomFieldList(config);
+  randomFieldList.generate();
+
+  diffRootInvRoot(randomFieldList);
+  diffRootRootInv(randomFieldList);
+  diffInvRootRoot(randomFieldList);
+  diffInvMult(randomFieldList);
+  diffMultInv(randomFieldList);
+}
+
+/**
+ * @brief 2D version of tests
+ */
+void test2d()
+{
+  Dune::ParameterTree config;
+  Dune::ParameterTreeParser parser;
+  parser.readINITree("randomfield2d.ini",config);
+
+  typedef GridTraits<double,double,2> GridTraits;
+
+  std::cout << "--------------" << std::endl;
+  std::cout << "2D Exponential" << std::endl;
+  std::cout << "--------------" << std::endl;
+  runTests<GridTraits,Dune::RandomField::ExponentialCovariance,INVMAT,INVROOT>(config);
+  std::cout << "--------------" << std::endl;
+  std::cout << "2D Gaussian   " << std::endl;
+  std::cout << "--------------" << std::endl;
+  runTests<GridTraits,Dune::RandomField::ExponentialCovariance,INVMAT,INVROOT>(config);
+  std::cout << "--------------" << std::endl;
+  std::cout << "2D Spherical  " << std::endl;
+  std::cout << "--------------" << std::endl;
+  runTests<GridTraits,Dune::RandomField::ExponentialCovariance,INVMAT,INVROOT>(config);
+}
+
+/**
+ * @brief 3D version of tests
+ */
+void test3d()
+{
+  Dune::ParameterTree config;
+  Dune::ParameterTreeParser parser;
+  parser.readINITree("randomfield3d.ini",config);
+
+  typedef GridTraits<double,double,3> GridTraits;
+
+  std::cout << "--------------" << std::endl;
+  std::cout << "3D Exponential" << std::endl;
+  std::cout << "--------------" << std::endl;
+  runTests<GridTraits,Dune::RandomField::ExponentialCovariance,INVMAT,INVROOT>(config);
+  std::cout << "--------------" << std::endl;
+  std::cout << "3D Gaussian   " << std::endl;
+  std::cout << "--------------" << std::endl;
+  runTests<GridTraits,Dune::RandomField::ExponentialCovariance,INVMAT,INVROOT>(config);
+  std::cout << "--------------" << std::endl;
+  std::cout << "3D Spherical  " << std::endl;
+  std::cout << "--------------" << std::endl;
+  runTests<GridTraits,Dune::RandomField::ExponentialCovariance,INVMAT,INVROOT>(config);
+}
 
 int main(int argc, char** argv)
 {
   try{
-    // Maybe initialize MPI
     Dune::MPIHelper& helper = Dune::MPIHelper::instance(argc, argv);
     std::cout << "Hello World! This is dune-randomfield." << std::endl;
     if(Dune::MPIHelper::isFake)
@@ -46,121 +203,21 @@ int main(int argc, char** argv)
       std::cout<<"I am rank "<<helper.rank()<<" of "<<helper.size()
         <<" processes!"<<std::endl;
 
-    Dune::ParameterTree config;
-    Dune::ParameterTreeParser parser;
-    parser.readINITree("randomfield.ini",config);
-
-    typedef GridTraits<double,double,2> GridTraits;
-    typedef Dune::RandomField::ExponentialCovariance Covariance;
-    
-    /*
-    GridTraits::Domain center;
-    center[0] = 0.5;
-    center[1] = 0.5;
-    randomFieldList.localize(center,0.15);
-    randomFieldList.writeToFile("sample");
-    Dune::RandomField::RandomFieldList<GridTraits,Covariance> copy(randomFieldList);
-    Dune::RandomField::RandomFieldList<GridTraits,Covariance> copy2(randomFieldList);
-    Dune::RandomField::RandomFieldList<GridTraits,Covariance> diff(randomFieldList);
-    copy.timesMatrixRoot();
-    copy.writeToFile("timesRoot");
-    copy.timesMatrixRoot();
-    copy.writeToFile("timesRootSquared");
-    diff = copy;
-    copy.timesInverseMatrix();
-    copy.writeToFile("rootRestored");
-    copy2.timesMatrix();
-    copy2.writeToFile("timesMatrix");
-    diff -= copy2;
-    diff.writeToFile("matrixDiff");
-    copy2.timesInverseMatrix();
-    copy2.writeToFile("matrixRestored");
-    randomFieldList -= copy;
-    randomFieldList.writeToFile("difference");
-    */
-
-    std::cout << "------------------------------" << std::endl;
-    std::cout << "root inv root" << std::endl;
-    std::cout << "------------------------------" << std::endl;
-    for (unsigned int i = 0; i < 10; i++)
-    {
-      Dune::RandomField::RandomFieldList<GridTraits,Covariance> randomFieldList(config);
-      randomFieldList.generateUncorrelated();
-      const double fieldNorm = std::sqrt(randomFieldList * randomFieldList);
-      Dune::RandomField::RandomFieldList<GridTraits,Covariance> copy(randomFieldList);
-      copy.timesMatrixRoot();
-      copy.timesInverseMatrix();
-      copy.timesMatrixRoot();
-      randomFieldList -= copy;
-      const double diffNorm = std::sqrt(randomFieldList * randomFieldList);
-      std::cout << "norm: " << fieldNorm << " diffNorm: " << diffNorm << " opNorm: " << diffNorm/fieldNorm << std::endl;
-    }
-
-    std::cout << "------------------------------" << std::endl;
-    std::cout << "root root inv" << std::endl;
-    std::cout << "------------------------------" << std::endl;
-    for (unsigned int i = 0; i < 10; i++)
-    {
-      Dune::RandomField::RandomFieldList<GridTraits,Covariance> randomFieldList(config);
-      randomFieldList.generateUncorrelated();
-      const double fieldNorm = std::sqrt(randomFieldList * randomFieldList);
-      Dune::RandomField::RandomFieldList<GridTraits,Covariance> copy(randomFieldList);
-      copy.timesMatrixRoot();
-      copy.timesMatrixRoot();
-      copy.timesInverseMatrix();
-      randomFieldList -= copy;
-      const double diffNorm = std::sqrt(randomFieldList * randomFieldList);
-      std::cout << "norm: " << fieldNorm << " diffNorm: " << diffNorm << " opNorm: " << diffNorm/fieldNorm << std::endl;
-    }
-
-    std::cout << "------------------------------" << std::endl;
-    std::cout << "root inv root local" << std::endl;
-    std::cout << "------------------------------" << std::endl;
-    for (unsigned int i = 0; i < 10; i++)
-    {
-      Dune::RandomField::RandomFieldList<GridTraits,Covariance> randomFieldList(config);
-      randomFieldList.generateUncorrelated();
-      GridTraits::Domain center;
-      center[0] = 0.5;
-      center[1] = 0.5;
-      randomFieldList.localize(center,0.15);
-      const double fieldNorm = std::sqrt(randomFieldList * randomFieldList);
-      Dune::RandomField::RandomFieldList<GridTraits,Covariance> copy(randomFieldList);
-      copy.timesMatrixRoot();
-      copy.timesInverseMatrix();
-      copy.timesMatrixRoot();
-      randomFieldList -= copy;
-      const double diffNorm = std::sqrt(randomFieldList * randomFieldList);
-      std::cout << "norm: " << fieldNorm << " diffNorm: " << diffNorm << " opNorm: " << diffNorm/fieldNorm << std::endl;
-    }
-
-    std::cout << "------------------------------" << std::endl;
-    std::cout << "root root inv local" << std::endl;
-    std::cout << "------------------------------" << std::endl;
-    for (unsigned int i = 0; i < 10; i++)
-    {
-      Dune::RandomField::RandomFieldList<GridTraits,Covariance> randomFieldList(config);
-      randomFieldList.generateUncorrelated();
-      GridTraits::Domain center;
-      center[0] = 0.5;
-      center[1] = 0.5;
-      randomFieldList.localize(center,0.15);
-      const double fieldNorm = std::sqrt(randomFieldList * randomFieldList);
-      Dune::RandomField::RandomFieldList<GridTraits,Covariance> copy(randomFieldList);
-      copy.timesMatrixRoot();
-      copy.timesMatrixRoot();
-      copy.timesInverseMatrix();
-      randomFieldList -= copy;
-      const double diffNorm = std::sqrt(randomFieldList * randomFieldList);
-      std::cout << "norm: " << fieldNorm << " diffNorm: " << diffNorm << " opNorm: " << diffNorm/fieldNorm << std::endl;
-    }
+    if (DIMENSION == 2)
+      test2d();
+    else if (DIMENSION == 3)
+      test3d();
+    else
+      DUNE_THROW(Dune::Exception,"only dimension 2 and 3 supported!");
 
     return 0;
   }
   catch (Dune::Exception &e){
     std::cerr << "Dune reported error: " << e << std::endl;
+    return 1;
   }
   catch (...){
     std::cerr << "Unknown exception thrown!" << std::endl;
+    return 1;
   }
 }
